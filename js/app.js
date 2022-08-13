27205e0a982b5e6657c7131df9c94da2a41f1446
Edwin Nord <edwin.nord09@gmail.com>
2022-08-13 12:35:37 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -2,25 +2,39 @@ const openForm = document.getElementById('form-submit');
 const form = document.getElementById('contact-form');
 const inputName = document.getElementById('typeText');
 const inputEmail = document.getElementById('typeEmail');
-const textarea = document.getElementById('textAreaExample');
+const textArea = document.getElementById('textAreaExample');
 const formCheck = document.getElementById('flexCheckDefault');
 const submitform = document.getElementById('formSend');
-const cancelForm = document.getElementById('cancel-form')
-const contact = document.querySelector('.contact-info')
+const cancelForm = document.getElementById('cancel-form');
+const contact = document.querySelector('.contact-info');
+const loading = document.getElementById('loading');
+
+
+
+
+
 
 document.getElementById('form-submit').onclick = function() {
     document.querySelector('.contact-info').style.display = 'none';
     document.getElementById('contact-form').style.display = "block";
     
+    
+    
 }
+
+
 
 document.getElementById('cancel-form').onclick = function() {
     document.getElementById('contact-form').style.display = 'none';
     document.querySelector('.contact-info').style.display = 'block';
+    
 }
 
 
 form.addEventListener('submit', (e)=> {
+
+    
+    
     let messages = [];
 
     if(inputName.value === '' || inputName.value == null) {
@@ -30,13 +44,15 @@ form.addEventListener('submit', (e)=> {
     if(inputEmail.value === '' || inputEmail.value == null) {
         messages.push('Your email is required')
     }
-    if(textarea.value === '') {
+    if(textArea.value === '') {
         messages.push('Please write us a message')
         alert('Please write us a message')
     }
 
     if(formCheck.checked) {
-        alert('Vous etes maintenant inscrit a notre liste de diffusion');
+        document.getElementById('loading').style.display = 'block'
+        setTimeout(loading,2000);
+        confirm('Vous etes maintenant inscrit a notre liste de diffusion');
     } else{
         e.preventDefault()
         alert('Please check box')
@@ -44,6 +60,8 @@ form.addEventListener('submit', (e)=> {
     if(messages.length > 0) {
         e.preventDefault()
     }
+    
+    
 })
 
 
